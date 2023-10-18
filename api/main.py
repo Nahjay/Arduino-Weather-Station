906a27a0977b98e5b794222c273082a5af97aea9
Nahjay <nahjaybattieste@gmail.com>
2023-10-17 22:50:35 -0400

    feat: added uvicorn to project

---
+++ api/main.py
@@ -0,0 +1,6 @@
+""" Create the API that will be used to interact with the data from the arduino and my website. """
+
+# Import the necessary modules
+from fastapi import FastAPI
+import uvicorn
+import serial
