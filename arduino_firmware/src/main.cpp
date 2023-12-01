9d3cdcca63321e703deae4c0d504294c0a7f0e8b
Nahjay <nahjaybattieste@gmail.com>
2023-11-30 21:10:46 -0500

    feat: can succesfully read humidity

--- arduino_firmware/src/main.cpp
+++ arduino_firmware/src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define TYPE DHT11
+int DHTPIN = 2;
+DHT dht(DHTPIN, TYPE);
+float temperature = 0;
+float humidity = 0;
+int setTime = 5000;
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  dht.begin();
+  delay(setTime);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
+
+    delay(2000);  // Delay for 2 seconds between readings
+
+    temperature = dht.readTemperature();
+    humidity = dht.readHumidity();
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(temperature) || isnan(humidity)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.print(" °C, Humidity: ");
+    Serial.print(humidity);
+    Serial.println(" %");
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
