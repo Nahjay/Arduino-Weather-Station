4bc32ba98dc37e29c3fc0fa4ddcce54b90c9f0f1
Nahjay <nahjaybattieste@gmail.com>
2023-12-05 22:36:55 -0500

    chore: refactored javascript file

--- web_interface/js/script.js
+++ web_interface/js/script.js
@@ -1,9 +1,7 @@
 // Create Functions that will be used in the web interface
 // These functions will be getting data from the server and displaying it on the web interface
 // They will be implementations of get requests to the server for all of the buttons on the web interface
-// This will be done using the fetch API 
-
-// Function to get the current temperature from the server and display it on the web interface
+// This will be done using the fetch API which is a modern replacement for XMLHttpRequest
 
 
 // Get all buttons within the web interface
